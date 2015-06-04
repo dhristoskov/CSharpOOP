47e0dd04d5dd2c72d7c69fdbf1c495b6d71fe378
DHristoskov <mike_mail@mail.bg>
2015-06-04 18:15:20 +0300

    ExceptionHandling

---
+++ ExceptionHandling/EnterNumbers_2/EnterNumbers_2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterNumbers_2
+{
+    class EnterNumbers_2
+    {
+        private static List<int> numbers = new List<int>();
+        private const int start = 1;
+        private const int end = 100;
+
+        private static int ReadNumbers()
+        {
+            string num = Console.ReadLine();
+            int number;
+            if (!int.TryParse(num, out number))
+            {
+                throw new FormatException();
+            }
+            else if (number <= 1 || number > 100)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            return number;
+        }
+        private static void PrintList()
+        {
+            numbers.Sort();
+            Console.WriteLine("Your 10 numbers");
+            Console.WriteLine(String.Join(", ",numbers));
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    int number = ReadNumbers();
+                    numbers.Add(number);
+                }
+                catch (FormatException)
+                {
+                    Console.Error.WriteLine("Enter the number in correct format!");
+                    i--;
+                }              
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.Error.WriteLine("End must be greater than start");
+                    i--;
+                }
+            }
+            PrintList();      
+        }       
+    }
+}
---
+++ ExceptionHandling/SquareRoot_1/SquareRoot_1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRoot_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 0;
+           try
+           {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                result = Math.Sqrt(number);
+                Console.WriteLine(result);
+           }       
+           catch (FormatException)
+           {
+               Console.Error.WriteLine("Enter correct format number!");
+           }
+           catch (OverflowException)
+           {
+               Console.Error.WriteLine("The number is too big for integer!");
+           }
+           catch(ArgumentOutOfRangeException)
+           {
+               Console.Error.WriteLine("Number can not be negative!");
+           }
+           finally
+           {
+               Console.WriteLine("Good bye");
+           }
+        }
+    }
+}
